5610a4b10495ff796ea11fdd842ac7d6c17b4a1f
Christina <christinaraganit@gmail.com>
2022-04-27 23:41:31 -0700

    :relieved:

--- smol-buddies/components/content.js
+++ smol-buddies/components/content.js
@@ -3,16 +3,113 @@ export default function Content() {
     <div className="content-container">
       <div className="be-a-buddy-container">
         <div className="heading">Be a Buddy</div>
-        <div className="be-a-buddy-content"></div>
+        <div className="be-a-buddy-content">
+          <div className="be-a-buddy-column">
+            <img className="buddy1" src="./border.png"></img>
+            <div>Trade items within an internal marketplace or P2P</div>
+          </div>
+
+          <div className="be-a-buddy-column">
+            <img className="buddy2" src="./border.png"></img>
+            <div>Craft using earned or staked materials and tokens</div>
+          </div>
+
+          <div className="be-a-buddy-column">
+            <img className="buddy3" src="./border.png"></img>
+            <div>Stake for $Budcoin which is paired with $magic</div>
+          </div>
+
+          <div className="be-a-buddy-column">
+            <img className="buddy4" src="./border.png"></img>
+            <div>Gain access to earn in our Minecraft server</div>
+          </div>
+        </div>
       </div>
       <div className="why-free-mint-container">
         <div className="heading">Why a Free mint?</div>
+        <div className="why-free-mint-row">
+          <div className="why-free-mint-column">
+            <div className="subheading">RAISING MONEY</div>
+            <div>
+              <p>
+                In NFT's some of the best and honest communities are through
+                free mints. Some of such are Smol Brains, Critterz, NFT Worlds,
+                BattleFly, Crypto Punks, and so on. We plan on generating our
+                project funds through the secondary marketplace, where we will
+                collect 5-10% of royalties.
+              </p>
+              <p>
+                We plan to use this money to hire full-time staff that range
+                from senior developers, product managers, to web designers, and
+                so on.
+              </p>
+              <p>
+                We want this project to always be building so raising enough
+                money to hire skillful people full-time will be crucial. We will
+                offer hourly wage positions so all work done is paid fairly.
+              </p>
+            </div>
+          </div>
+          <div className="why-free-mint-column">
+            <img className="buddy4" src="./border.png"></img>
+          </div>
+        </div>
+
+        <div className="why-free-mint-row">
+          <div className="why-free-mint-column">
+            <div className="subheading">BUILDING TRUST</div>
+            <div>
+              <p>
+                Many projects will charge a mint fee, acquiring tons of cash,
+                then leaving the community to worry about their money. Often
+                poorly updating their community, most of the time it's just poor
+                communication.
+              </p>
+              <p>
+                What is raised initially will determine how much faith the
+                community has put into us. Regardless of the amount raised, we
+                will follow through with our planning and will be working with
+                our community to build what we envision together.
+              </p>
+            </div>
+          </div>
+          <div className="why-free-mint-column">
+            <img className="buddy3" src="./border.png"></img>
+          </div>
+        </div>
       </div>
+
       <div className="faq-container">
         <div className="heading">Frequently asked questions</div>
       </div>
+
       <div className="meet-the-team-container">
         <div className="heading">Meet the team</div>
+        <div className="meet-the-team-content">
+          <div className="meet-the-team-column">
+            <img className="buddy3" src="./border.png"></img>
+            <div className="name">Name name</div>
+            <div className="title">Bubble tea drinker</div>
+          </div>
+
+          <div className="meet-the-team-column">
+            <img className="buddy2" src="./border.png"></img>
+            <div className="name">Name name</div>
+            <div className="title">Bubble tea drinker</div>
+          </div>
+
+          <div className="meet-the-team-column">
+            <img className="buddy4" src="./border.png"></img>
+            <div className="name">Name name</div>
+            <div className="title">Bubble tea drinker</div>
+          </div>
+
+          <div className="meet-the-team-column">
+            <img className="buddy1" src="./border.png"></img>
+            <div className="name">Name name</div>
+            <div className="title">Bubble tea drinker</div>
+          </div>
+        </div>
       </div>
     </div>
   );
