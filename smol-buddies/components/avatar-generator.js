7b73e3b58c4731d12593f17344137c4e7b2d8591
Eric Dam <dam.eric3@gmail.com>
2022-07-05 22:09:13 -0700

    finished prototype of avatar generation

--- smol-buddies/components/avatar-generator.js
+++ smol-buddies/components/avatar-generator.js
@@ -6,33 +6,85 @@ import pixelBackground1 from '../public/pixel_background_1.png';
 import pixelBackground2 from '../public/pixel_background_2.png';
 import pixelHat1 from '../public/pixel_hat_1.png';
 import pixelHat2 from '../public/pixel_hat_2.png';
+import monke from '../public/monke.png';
+import muscleBody from '../public/muscle_body.png';
+import legs from '../public/legs.png';
 
 const backgrounds = [pixelBackground1, pixelBackground2];
 const hats = [pixelHat1, pixelHat2];
+const monkes = [monke];
+const shirts = [muscleBody];
+const pants = [legs];
 
 export default function AvatarGenerator() {
-    // const [option, setOption] = new useState("color");
+    const [option, setOption] = new useState("color");
     const [listOfOptions, setListOfOptions] = new useState(backgrounds);
     
     const setSelection = (e, choice) => {
         const isSelected = e.currentTarget.className === 'selected';
         if (!isSelected) {
-            // setOption(choice);
+            setOption(choice);
             const divs = document.getElementsByClassName('selected');
             for (let i = 0; i < divs.length; i++) {
                 divs[i].classList.remove('selected');
             }
             e.currentTarget.className = 'selected';
-            if (choice === 'background') {
+            if (choice === 'color') {
+                setListOfOptions(monkes);
+            } else if (choice === 'background') {
                 setListOfOptions(backgrounds);
             } else if (choice === 'hat') {
                 setListOfOptions(hats);
+            } else if (choice === 'shirt') {
+                setListOfOptions(shirts);
+            } else if (choice === 'pants') {
+                setListOfOptions(pants);
             }
         }
     }
+
+    const changeAvatar = (e) => {
+        const selected = e.currentTarget.style.backgroundImage;
+        if (option === 'color') {
+            const color = document.getElementById("color");
+            color.style.backgroundImage = selected;
+            color.style.backgroundSize = "contain";
+            color.style.backgroundRepeat = "no-repeat";
+            color.style.backgroundPosition = "center";
+        } else if (option === "background") {
+            const avatar = document.getElementById("avatar");
+            avatar.style.backgroundImage = selected;
+            avatar.style.backgroundSize = "cover";
+            avatar.style.backgroundRepeat = "no-repeat";
+        } else if (option === "hat") {
+            const hat = document.getElementById("hat");
+            hat.style.backgroundImage = selected;
+            hat.style.backgroundSize = "contain";
+            hat.style.backgroundRepeat = "no-repeat";
+            hat.style.backgroundPosition = "center";
+        } else if (option === "shirt") {
+            const shirt = document.getElementById("shirt");
+            shirt.style.backgroundImage = selected;
+            shirt.style.backgroundSize = "contain";
+            shirt.style.backgroundRepeat = "no-repeat";
+            shirt.style.backgroundPosition = "center";
+        } else if (option === "pants") {
+            const pants = document.getElementById("pants");
+            pants.style.backgroundImage = selected;
+            pants.style.backgroundSize = "contain";
+            pants.style.backgroundRepeat = "no-repeat";
+            pants.style.backgroundPosition = "center";
+        }
+    }
+
     return (
         <div className="generator-container">
-            <div className="avatar"></div>
+            <div id="avatar">
+                <div id="hat"></div>
+                <div id="color"></div>
+                <div id="shirt"></div>
+                <div id="pants"></div>
+            </div>
             <div className="avatar-options-select">
                 <div className='selected' onClick={e => setSelection(e, "color")}>
                     <IoColorPaletteSharp size={24}></IoColorPaletteSharp>
@@ -56,10 +108,11 @@ export default function AvatarGenerator() {
                     {listOfOptions.map(photo => {
                         return <div key={photo.src} className='option-div' style={{
                             backgroundImage: `url(${photo.src})`,
-                            backgroundSize: 'cover',
-                            backgroundRepeat: 'no-repeat'
+                            backgroundSize: 'contain',
+                            backgroundRepeat: 'no-repeat',
+                            backgroundPosition: 'center'
                             }}
-                            
+                            onClick={e => changeAvatar(e)}
                         >
 
                         </div>
